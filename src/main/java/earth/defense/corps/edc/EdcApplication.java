83fff46a0532be94c0abb87d6f4cc9b8fed58afb
Brian <bwook9908@gmail.com>
2023-03-17 22:17:51 +0900

    add project

---
+++ src/main/java/earth/defense/corps/edc/EdcApplication.java
@@ -0,0 +1,13 @@
+package earth.defense.corps.edc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/earth/defense/corps/edc/EdcApplicationTests.java
@@ -0,0 +1,13 @@
+package earth.defense.corps.edc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
