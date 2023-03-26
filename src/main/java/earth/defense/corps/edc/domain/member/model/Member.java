751ef743581c31a85d5364d116340e834ddf9da3
Brian <bwook9908@gmail.com>
2023-03-27 04:57:48 +0900

    feat: stage 도메인 추가

--- src/main/java/earth/defense/corps/edc/domain/member/model/Member.java
+++ src/main/java/earth/defense/corps/edc/domain/member/model/Member.java
@@ -21,18 +21,16 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Lob
     @Column(name = "MEMBER_EMAIL", nullable = false, unique = true)
     private String email;
 
-    @Lob
-    @Column(name = "MEMBER_NAME",nullable = false, unique = true)
+    @Column(name = "MEMBER_NAME", nullable = false, unique = true)
     private String name;
 
     @Column(name = "POSSESSING_GOLD")
     private int possessing_gold;
 
-    @Column(name ="POSSESSING_JEM")
+    @Column(name = "POSSESSING_JEM")
     private int possessing_jem;
 
 
@@ -40,21 +38,20 @@ public class Member {
     @JoinColumn(name = "CHARACTER_ID", insertable = false, updatable = false)
     private CharacterStat characterStat;
 
-    /**
-     * need to be modified
-     */
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<Stage> stage_clear_list =new ArrayList<Stage>();
 
-    private Member(String name, String email, int gold, int jem){
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    private final List<Stage> stage_clear_list = new ArrayList<Stage>();
+
+    private Member(String name, String email, int gold, int jem, CharacterStat characterStat) {
         this.name = name;
         this.email = email;
-        this.possessing_gold=gold;
+        this.possessing_gold = gold;
         this.possessing_jem=jem;
+        this.characterStat = characterStat;
     }
 
-    public static Member of(String name, String email, int gold, int jem) {
-        return new Member(name, email, gold, jem);
+    public static Member of(String name, String email, int gold, int jem, CharacterStat characterStat) {
+        return new Member(name, email, gold, jem, characterStat);
     }
 
     public void modifyMemberInfo(String name) {
