0507192e611a6bed7ad7a096b994fc1371c9ae92
chosunghyun18 <krismon@naver.com>
2023-05-13 18:21:32 +0900

    fix : remove useless repository

--- src/main/java/earth/defense/corps/edc/domain/item/repository/ShoesRepository.java
+++
@@ -1,7 +0,0 @@
-package earth.defense.corps.edc.domain.item.repository;
-
-import earth.defense.corps.edc.domain.item.model.Shoes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoesRepository extends JpaRepository<Shoes,Long> {
-}
