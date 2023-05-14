fb0991fa52469b5a3d946983cc7f2fe84a6cd606
chosunghyun18 <krismon@naver.com>
2023-05-14 16:43:28 +0900

    feat : add inventory constructor

--- src/main/java/earth/defense/corps/edc/domain/inventory/model/Inventory.java
+++ src/main/java/earth/defense/corps/edc/domain/inventory/model/Inventory.java
@@ -1,11 +1,13 @@
 package earth.defense.corps.edc.domain.inventory.model;
 
 import earth.defense.corps.edc.domain.item.model.BaseItem;
+import earth.defense.corps.edc.domain.member.model.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,4 +25,11 @@ public class Inventory {
     @OneToMany(mappedBy ="inventory")
     private List<BaseItem> items =new ArrayList<>();
 
+    @OneToOne
+    private Member member;
+
+    protected Inventory(){}
+//    public Inventory(Member member){
+//
+//    }
 }
