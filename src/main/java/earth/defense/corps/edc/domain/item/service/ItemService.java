99e2f3da5db586e75b03375fc39d9902654ae41a
Brian <bwook9908@gmail.com>
2023-05-21 00:42:01 +0900

    feat: item save function add

--- src/main/java/earth/defense/corps/edc/domain/item/service/ItemService.java
+++ src/main/java/earth/defense/corps/edc/domain/item/service/ItemService.java
@@ -1,7 +1,7 @@
 package earth.defense.corps.edc.domain.item.service;
 import earth.defense.corps.edc.domain.item.dto.request.ItemRegisterRequest;
 import earth.defense.corps.edc.domain.item.dto.response.ItemRegisterResponse;
-import earth.defense.corps.edc.domain.item.model.BaseItem;
+import earth.defense.corps.edc.domain.item.model.*;
 import earth.defense.corps.edc.domain.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,13 +14,34 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     @Transactional
-    public void save(BaseItem baseItem){
-        itemRepository.save(baseItem);
+    public void save(String type, ItemRegisterRequest request){
+        ItemType itm = ItemType.valueOf(type);
+        switch (itm) {
+            case ARMOR:
+                itemRepository.save(Armor.of(type, request));
+                break;
+            case GLOVES:
+                itemRepository.save(Gloves.of(type, request));
+                break;
+            case HELMET:
+                itemRepository.save(Helmet.of(type, request));
+                break;
+            case SHIELD:
+                itemRepository.save(Shield.of(type, request));
+            case SHOES:
+                itemRepository.save(Shoes.of(type, request));
+            case WEAPON:
+                itemRepository.save(Weapon.of(type, request));
+        }
     }
+
+
+
     @Transactional
     public ItemRegisterResponse saveItem(String type, ItemRegisterRequest request){
-        save(BaseItem.of(type, request));
+        save(type, request);
         ItemRegisterResponse itemRegisterResponse = new ItemRegisterResponse();
         return itemRegisterResponse;
     }
+
 }
