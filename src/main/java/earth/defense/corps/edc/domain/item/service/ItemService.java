2b1eb7720708f527d84efbf706fa4d96de7a9426
chosunghyun18 <krismon@naver.com>
2023-05-23 14:13:24 +0900

    chore : follow by google java convention

--- src/main/java/earth/defense/corps/edc/domain/item/service/ItemService.java
+++ src/main/java/earth/defense/corps/edc/domain/item/service/ItemService.java
@@ -1,4 +1,5 @@
 package earth.defense.corps.edc.domain.item.service;
+
 import earth.defense.corps.edc.domain.item.dto.request.ItemRegisterRequest;
 import earth.defense.corps.edc.domain.item.dto.request.ItemUpgradeRequest;
 import earth.defense.corps.edc.domain.item.dto.response.ItemDeleteResponse;
@@ -10,7 +11,9 @@ import earth.defense.corps.edc.domain.item.repository.*;
 import earth.defense.corps.edc.domain.member.model.Member;
 import earth.defense.corps.edc.domain.member.service.MemberService;
 import earth.defense.corps.edc.global.ResponseHeader;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,47 +23,68 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
     private final ItemRepository itemRepository;
     private final MemberService memberService;
+
     @Transactional
-    public Long save(String type, ItemRegisterRequest request, Long memberId){
-        ItemType itm = ItemType.valueOf(type);
+    public Long save(String type, ItemRegisterRequest request, Long memberId) {
+        ItemType itemType = ItemType.valueOf(type);
         Member member = memberService.getMemberById(memberId);
-        BaseItem savedItem = switch (itm) {
-            case ARMOR -> itemRepository.save(Armor.of(type, request, member));
-            case GLOVES -> itemRepository.save(Gloves.of(type, request, member));
-            case HELMET -> itemRepository.save(Helmet.of(type, request, member));
-            case SHIELD -> itemRepository.save(Shield.of(type, request, member));
-            case SHOES -> itemRepository.save(Shoes.of(type, request, member));
-            case WEAPON -> itemRepository.save(Weapon.of(type, request, member));
-        };
+        BaseItem savedItem;
+
+        switch (itemType) {
+            case ARMOR:
+                savedItem = itemRepository.save(Armor.of(type, request, member));
+                break;
+            case GLOVES:
+                savedItem = itemRepository.save(Gloves.of(type, request, member));
+                break;
+            case HELMET:
+                savedItem = itemRepository.save(Helmet.of(type, request, member));
+                break;
+            case SHIELD:
+                savedItem = itemRepository.save(Shield.of(type, request, member));
+                break;
+            case SHOES:
+                savedItem = itemRepository.save(Shoes.of(type, request, member));
+                break;
+            case WEAPON:
+                savedItem = itemRepository.save(Weapon.of(type, request, member));
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid item type: " + type);
+        }
+
         return savedItem.getId();
     }
+
     @Transactional
-    public ItemRegisterResponse saveItem(String type, ItemRegisterRequest request){
+    public ItemRegisterResponse saveItem(String type, ItemRegisterRequest request) {
         Long id = save(type, request, request.getMemberId());
         return new ItemRegisterResponse(id, new ResponseHeader(200, "아이템 등록 성공"));
     }
-    public BaseItem getById(Long ItemId){
-        return itemRepository.findById(ItemId).orElseThrow();
+
+    public BaseItem getById(Long itemId) {
+        return itemRepository.findById(itemId).orElseThrow();
     }
-    public ItemListResponse getItemList(Long memberId){
+
+    public ItemListResponse getItemList(Long memberId) {
         Member member = memberService.getMemberById(memberId);
         return new ItemListResponse(itemRepository.findAllByMember(member), new ResponseHeader(200, "아이템 리스트 불러오기 성공"));
     }
 
     @Transactional
-    public ItemUpgradeResponse upgradeItem(Long ItemId, ItemUpgradeRequest request){
-        BaseItem item = getById(ItemId);
+    public ItemUpgradeResponse upgradeItem(Long itemId, ItemUpgradeRequest request) {
+        BaseItem item = getById(itemId);
         item.itemUpgrade(request);
         return new ItemUpgradeResponse(item, new ResponseHeader(200, "아이템 수정(강화) 완료"));
     }
+
     @Transactional
     public ItemDeleteResponse deleteItem(Long itemId) {
         BaseItem item = itemRepository.findById(itemId).orElseThrow();
-        Member member  = item.getMember();
-        int currentGold = member.getPossessing_gold();
+        Member member = item.getMember();
+        int currentGold = member.getPossessingGold();
         member.modifyMemberGold(currentGold + item.getPrice());
         itemRepository.deleteById(itemId);
         return new ItemDeleteResponse(new ResponseHeader(200, "아이템 삭제 완료"));
     }
-
 }
