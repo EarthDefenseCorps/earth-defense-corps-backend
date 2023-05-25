35fa55d6d8a6c8b61ac7d4b4bc61a2b2c0105b87
chosunghyun18 <krismon@naver.com>
2023-05-25 16:33:22 +0900

    fix : add member info

--- src/main/java/earth/defense/corps/edc/domain/item/dto/response/ItemListResponse.java
+++ src/main/java/earth/defense/corps/edc/domain/item/dto/response/ItemListResponse.java
@@ -1,5 +1,6 @@
 package earth.defense.corps.edc.domain.item.dto.response;
 
+import earth.defense.corps.edc.domain.item.dto.response.ItemResponse.ItemResponseDto;
 import earth.defense.corps.edc.domain.item.model.BaseItem;
 import earth.defense.corps.edc.global.ResponseHeader;
 import java.util.stream.Collectors;
@@ -11,7 +12,7 @@ import java.util.List;
 public class ItemListResponse {
     private final ResponseHeader header;
     private List<BaseItem> itemList;
-    private List<ItemResponse> items;
+    private List<ItemResponseDto> items;
 
     public ItemListResponse(List<BaseItem> itemList, ResponseHeader header) {
         this.header = header;
@@ -19,6 +20,6 @@ public class ItemListResponse {
     }
     public ItemListResponse( ResponseHeader header,List<BaseItem> itemList) {
         this.header = header;
-        this.items = itemList.stream().map(ItemResponse::new).collect(Collectors.toList());
+        this.items = itemList.stream().map(ItemResponseDto::new).collect(Collectors.toList());
     }
 }
